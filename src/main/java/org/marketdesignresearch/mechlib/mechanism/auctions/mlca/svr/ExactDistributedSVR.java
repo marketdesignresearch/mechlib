9f08dff5b244dd20432cf23844108b7f5af0973e
manuel <manuel@MOZART.lan>
2020-09-02 18:16:45 +0200

    add missing SVR functions

--- src/main/java/org/marketdesignresearch/mechlib/mechanism/auctions/mlca/svr/ExactDistributedSVR.java
+++ src/main/java/org/marketdesignresearch/mechlib/mechanism/auctions/mlca/svr/ExactDistributedSVR.java
@@ -1,7 +1,6 @@
 package org.marketdesignresearch.mechlib.mechanism.auctions.mlca.svr;
 
 import org.marketdesignresearch.mechlib.core.bid.bundle.BundleExactValueBids;
-import org.marketdesignresearch.mechlib.mechanism.auctions.mlca.MachineLearningAllocationInferrer;
 import org.marketdesignresearch.mechlib.mechanism.auctions.mlca.svr.kernels.Kernel;
 
 public class ExactDistributedSVR extends DistributedSVR<BundleExactValueBids> {
@@ -15,7 +14,7 @@ public class ExactDistributedSVR extends DistributedSVR<BundleExactValueBids> {
 	}
 
 	@Override
-	public MachineLearningAllocationInferrer getMLFunction(BundleExactValueBids bids) {
+	public ExactSupportVector getMLFunction(BundleExactValueBids bids) {
 		return new ExactSupportVector(this.getSetup(), bids, this.getMipInstrumentation());
 	}
 }
--- src/main/java/org/marketdesignresearch/mechlib/mechanism/auctions/mlca/svr/SupportVector.java
+++ src/main/java/org/marketdesignresearch/mechlib/mechanism/auctions/mlca/svr/SupportVector.java
@@ -1,5 +1,6 @@
 package org.marketdesignresearch.mechlib.mechanism.auctions.mlca.svr;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Set;
 
@@ -7,6 +8,7 @@ import org.marketdesignresearch.mechlib.core.Allocation;
 import org.marketdesignresearch.mechlib.core.Bundle;
 import org.marketdesignresearch.mechlib.core.Domain;
 import org.marketdesignresearch.mechlib.core.bid.bundle.BundleExactValueBids;
+import org.marketdesignresearch.mechlib.core.bid.bundle.BundleExactValuePair;
 import org.marketdesignresearch.mechlib.core.bid.bundle.BundleValueBid;
 import org.marketdesignresearch.mechlib.core.bid.bundle.BundleValueBids;
 import org.marketdesignresearch.mechlib.core.bidder.Bidder;
@@ -22,6 +24,7 @@ import lombok.Setter;
 public abstract class SupportVector<B extends BundleValueBid<?>, T extends BundleValueBids<B>>
 		implements MachineLearningAllocationInferrer, MipInstrumentationable {
 
+	@Getter
 	private final BundleExactValueBids supportVectorsPerBider;
 	private final Kernel kernel;
 	@Getter
@@ -44,6 +47,15 @@ public abstract class SupportVector<B extends BundleValueBid<?>, T extends Bundl
 	public Allocation getInferredEfficientAllocation(Domain domain, ElicitationEconomy economy,
 			Map<Bidder, Set<Bundle>> excludedBids) {
 		return this.kernel.getAllocationWithExcludedBundles(domain, economy, this.supportVectorsPerBider, excludedBids);
+	}
+	
+	public double getModelPredictedValueOf(Bidder bidder, Bundle bundle) {
+		if (supportVectorsPerBider==null || supportVectorsPerBider.getBid(bidder) == null) return 0d;
+		Double value = 0.0;
+		for (BundleExactValuePair bv : supportVectorsPerBider.getBid(bidder).getBundleBids()){
+			value+=bv.getAmount().doubleValue()*kernel.getValue(bv.getBundle(),bundle);
+		}
+		return value;
 	}
 
 	protected abstract SupportVectorMIP<B> createSupportVectorMIP(SupportVectorSetup setup, B bid);
